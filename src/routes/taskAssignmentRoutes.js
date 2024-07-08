c41df4859661ec91527a78becaf085348cd307ff
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2024-07-08 19:44:31 +0700

    Add api docs for last schema

--- src/routes/taskAssignmentRoutes.js
+++ src/routes/taskAssignmentRoutes.js
@@ -4,10 +4,144 @@ const configJwt = require('../config/configJwt');
 const { authorizeRole } = require('../config/configAuthRole');
 const {createTaskAssignment, getTaskAssignmentById, getAllTaskAssignment, updateTaskAssignment, deleteTaskAssignment} = require('../controllers/taskAssignmentController')
 
+/** POST Methods */
+/**
+ * @openapi
+ * '/api/v1/taskAssignment/create':
+ *  post:
+ *     tags:
+ *     - Task Assignment
+ *     summary: Create a Task Assignment
+ *     security:
+ *     - BearerAuth: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - taskList_id
+ *              - member_id
+ *            properties:
+ *              taskList_id:
+ *                type: string
+ *              member_id:
+ *                type: string
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.post("/create", configJwt.checkTokenVerify, authorizeRole('admin'), createTaskAssignment)
+
+/** GET Methods */
+/**
+ * @openapi
+ * '/api/v1/taskAssignment/{:_id}':
+ *  get:
+ *     tags:
+ *     - Task Assignment
+ *     summary: Get One Task Assignment
+ *     parameters:
+ *      - name: id task assignment
+ *        in: path
+ *        required: true
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Get One Item Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.get("/:_id", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), getTaskAssignmentById)
+
+/** GET Methods */
+/**
+ * @openapi
+ * '/api/v1/taskAssignment/':
+ *  get:
+ *     tags:
+ *     - Task Assignment
+ *     summary: Get All Task Assignment
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Get All Item Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.get("/", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), getAllTaskAssignment)
+
+/** PUT Methods */
+/**
+ * @openapi
+ * '/api/v1/taskAssignment/{:_id}':
+ *  put:
+ *     tags:
+ *     - Task Assignment
+ *     summary: Update a Task Assignment
+ *     parameters:
+ *      - name: id task assignment
+ *        in: path
+ *        required: true
+ *     security:
+ *     - BearerAuth: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - taskList_id
+ *              - member_id
+ *            properties:
+ *              taskList_id:
+ *                type: string
+ *              member_id:
+ *                type: string
+ *     responses:
+ *      201:
+ *        description: Updated
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.put("/:_id", configJwt.checkTokenVerify, authorizeRole('admin'), updateTaskAssignment)
+
+/** DELETE Methods */
+/**
+ * @openapi
+ * '/api/v1/taskAssignment/{:_id}':
+ *  delete:
+ *     tags:
+ *     - Task Assignment
+ *     summary: Delete One Task Assignment
+ *     parameters:
+ *      - name: id task Assignment
+ *        in: path
+ *        required: true
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Delete One Task Assignment Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.delete("/:_id", configJwt.checkTokenVerify, authorizeRole('admin'), deleteTaskAssignment)
 
 
--- src/routes/taskListRoutes.js
+++ src/routes/taskListRoutes.js
@@ -4,10 +4,175 @@ const configJwt = require('../config/configJwt');
 const { authorizeRole } = require('../config/configAuthRole');
 const {createOneTaskList, getOneTaskList, getAllTaskList, updateOneTaskList, deleteTaskList} = require('../controllers/taskListController')
 
+
+/** POST Methods */
+/**
+ * @openapi
+ * '/api/v1/taskList/create':
+ *  post:
+ *     tags:
+ *     - Task List
+ *     summary: Create a Task List
+ *     security:
+ *     - BearerAuth: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - description
+ *              - status
+ *              - priority
+ *              - startdate
+ *              - duedate
+ *              - boardList
+ *            properties:
+ *              title:
+ *                type: string
+ *              description:
+ *                type: string
+ *              status:
+ *                type: string
+ *              priority:
+ *                type: string
+ *              startdate:
+ *                type: string
+ *              duedate:
+ *                type: string
+ *              boardList_id:
+ *                type: string
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.post("/create", configJwt.checkTokenVerify, authorizeRole('admin'), createOneTaskList)
+
+/** GET Methods */
+/**
+ * @openapi
+ * '/api/v1/taskList/{:_id}':
+ *  get:
+ *     tags:
+ *     - Task List
+ *     summary: Get One Task List
+ *     parameters:
+ *      - name: id task list
+ *        in: path
+ *        required: true
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Get One Item Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.get("/:_id", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), getOneTaskList)
+
+/** GET Methods */
+/**
+ * @openapi
+ * '/api/v1/taskList/':
+ *  get:
+ *     tags:
+ *     - Task List
+ *     summary: Get All Task List
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Get All Item Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.get("/", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), getAllTaskList)
+
+/** PUT Methods */
+/**
+ * @openapi
+ * '/api/v1/taskList/{:_id}':
+ *  put:
+ *     tags:
+ *     - Task List
+ *     summary: Update a Task List
+ *     parameters:
+ *     - name: id task list
+ *       in: path
+ *       required: true
+ *     security:
+ *     - BearerAuth: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - description
+ *              - status
+ *              - priority
+ *              - startdate
+ *              - duedate
+ *              - boardList
+ *            properties:
+ *              title:
+ *                type: string
+ *              description:
+ *                type: string
+ *              status:
+ *                type: string
+ *              priority:
+ *                type: string
+ *              startdate:
+ *                type: string
+ *              duedate:
+ *                type: string
+ *              boardList_id:
+ *                type: string
+ *     responses:
+ *      201:
+ *        description: Updated
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.put("/:_id", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), updateOneTaskList)
+
+/** DELETE Methods */
+/**
+ * @openapi
+ * '/api/v1/taskList/{:_id}':
+ *  delete:
+ *     tags:
+ *     - Task List
+ *     summary: Delete One Task List
+ *     parameters:
+ *      - name: id task list
+ *        in: path
+ *        required: true
+ *     security:
+ *     - BearerAuth: []
+ *     responses:
+ *      200:
+ *        description: Delete One Task List Successfull
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 routes.delete("/:_id", configJwt.checkTokenVerify, authorizeRole('admin', 'user'), deleteTaskList)
 
 module.exports = routes;
