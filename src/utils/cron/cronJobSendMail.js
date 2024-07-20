c4e8e312ff18f73cbd6a43c086abf8469c81c7d5
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2024-07-20 18:50:59 +0700

    Cron Job is done but must fix template mail more beautifull

--- src/utils/cron/cronJobSendMail.js
+++ src/utils/cron/cronJobSendMail.js
@@ -17,63 +17,38 @@ const cronAutoDoJon = {
           duedate: { $gte: now, $lte: taskUpComingDeadline },
         });
 
-        // const tasksByMember = {};
+        const tasksByMember = {};
 
-        // for(let task of taskListsDeadline) {
-        //     const taskAssignment = await Model.taskAssignmentModel.findOne({taskList: task._id}).populate('member').exec()
-        //     if(taskAssignment) {
-        //         const memberId = taskAssignment.member._id
-        //         if(!tasksByMember[memberId]) {
-        //             tasksByMember[memberId] = {
-        //                 member: await Model.membersModel.findById(memberId).populate('user').exec(),
-        //                 task: []
-        //             };
-        //         }
-        //         tasksByMember[memberId].taskListsDeadline.push(task)
+        for(let task of taskListsDeadline) {
+            const taskAssignment = await Model.taskAssignmentModel.findOne({taskList: task._id}).populate('member').exec()
+            if(taskAssignment) {
+                const memberId = taskAssignment.member._id
+                if(!tasksByMember[memberId]) {
+                    tasksByMember[memberId] = {
+                        member: await Model.membersModel.findById(memberId).populate('user').exec(),
+                        tasks: []
+                    };
+                }
+                tasksByMember[memberId].tasks.push(task)
 
-        //     }
-        // }
-
-        // for(let memberId of Object.keys(tasksByMember)) {
-        //     const member = tasksByMember[memberId].member;
-        //     const tasks = tasksByMember[memberId].tasks;
-        //     if(member && member.user && member.user.email) {
-        //         const emailUser = member.user.email
-        //         const taskDetail = {
-        //             name: member.user.name,
-        //             tasks: tasks.map(task => ({
-        //                 title: task.title,
-        //                 description: task.description,
-        //                 duedate: format(new Date(task.duedate), 'dd/MM/yyyy HH:mm:ss', {locale: vi})
-        //             }))
-        //         };
-        //         await createMail.sendMailTask(emailUser, taskDetail)
-        //     }
-        // }
-
-        for (let task of taskListsDeadline) {
-          const taskAssignment = await Model.taskAssignmentModel
-            .findOne({ taskList: task._id })
-            .populate('member')
-            .exec();
-          if (taskAssignment) {
-            const member = await Model.membersModel
-              .findById(taskAssignment.member._id)
-              .populate('user')
-              .exec();
-            if (member && member.user && member.user.email) {
-              const emailUser = member.user.email;
-              const taskDetail = {
-                name: member.user.name,
-                tasks: taskListsDeadline.map(task => ({
-                    title: task.title,
-                    description: task.description,
-                    duedate: format(new Date(task.duedate), 'dd/MM/yyyy HH:mm:ss', {locale: vi})
-                }))
-              };
-              createMail.sendMailTask(emailUser, taskDetail);
             }
-          }
+        }
+
+        for(let memberId of Object.keys(tasksByMember)) {
+            const member = tasksByMember[memberId].member;
+            const tasks = tasksByMember[memberId].tasks;
+            if(member && member.user && member.user.email) {
+                const emailUser = member.user.email
+                const taskDetail = {
+                    name: member.user.name,
+                    tasks: tasks.map(task => ({
+                        title: task.title,
+                        description: task.description,
+                        duedate: format(new Date(task.duedate), 'dd/MM/yyyy HH:mm:ss', {locale: vi})
+                    }))
+                };
+                await createMail.sendMailTask(emailUser, taskDetail)
+            }
         }
       } catch (error) {
         console.error('Error fetching task or sending mails', error);
